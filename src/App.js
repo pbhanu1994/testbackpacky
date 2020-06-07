28a42006bc2b4e2759b2ee42441b7d3eeae52dd2
Bhanu <pbhanu.1994@gmail.com>
2020-06-08 01:56:20 +1000

    JSA

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS
+          Learn React JSA
         </a>
       </header>
     </div>
