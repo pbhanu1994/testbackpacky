91606b7adc1d3d0c1e3be01acf6a6ca9987fb676
Bhanu <pbhanu.1994@gmail.com>
2020-07-25 15:55:25 +1000

    Added the line SRM University

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello SRM!</p>
+        <p>Hello SRM University!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
